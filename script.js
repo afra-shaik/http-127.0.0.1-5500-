9768d4c39a0d4ab419cc9711e3149001f4a7ca01
Afra Shaik <afrashaik02@gmail.com>
2026-07-29 23:36:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  const song = document.getElementById("birthday-song");
-  document.body.addEventListener("click", () => {
-    song.play().catch((e) => console.log("User must click to play:", e));
-  });
-};
